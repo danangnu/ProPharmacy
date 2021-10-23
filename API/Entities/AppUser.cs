0c0c31748f4a0d2432a9731bd0e7d6dfd449b2aa
Danang Nurdiansyah <nattesc82@gmail.com>
2021-10-23 15:47:17 +0800

    master

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
