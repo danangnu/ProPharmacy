f211fa41e4f280a99fee6206d4181235356a220d
Danang Nurdiansyah <nattesc82@gmail.com>
2021-11-02 17:08:10 +0800

    master

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -12,5 +12,6 @@ namespace API.Data
         public DbSet<AppUser> Users { get; set; }
         public DbSet<FilesVersion> FilesVersion { get; set; }
         public DbSet<Docs> Documents { get; set; }
+        public DbSet<Prescriptions> Prescriptions { get; set; }
     }
 }
