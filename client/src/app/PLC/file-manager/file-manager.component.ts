a2a23043aa3aec139d8a34bbbce6c191ea71b29c
Danang Nurdiansyah <nattesc82@gmail.com>
2021-11-21 12:09:35 +0800

    master

--- client/src/app/PLC/file-manager/file-manager.component.ts
+++ client/src/app/PLC/file-manager/file-manager.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 import { FileUploader } from 'ng2-file-upload';
-import { NgxSpinnerService, Spinner } from 'ngx-spinner';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { take } from 'rxjs/operators';
 import { FileVersion } from 'src/app/_models/fileVersion';
 import { User } from 'src/app/_models/user';
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-file-manager',
   templateUrl: './file-manager.component.html',
-  styleUrls: ['./file-manager.component.css']
+  styleUrls: ['./file-manager.component.css'],
 })
 export class FileManagerComponent implements OnInit {
   @Input() fileVersion: FileVersion;
@@ -19,10 +19,13 @@ export class FileManagerComponent implements OnInit {
   hasBaseDropzoneOver = false;
   baseUrl = environment.backendUrl;
   user: User;
-  
-  constructor(private route: ActivatedRoute,
-              private auth: AuthService,
-              private spinnerService: NgxSpinnerService) { }
+  done: boolean;
+
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService,
+    private spinnerService: NgxSpinnerService
+  ) {}
 
   ngOnInit(): void {
     this.initializeUploader();
@@ -32,33 +35,48 @@ export class FileManagerComponent implements OnInit {
     this.hasBaseDropzoneOver = e;
   }
 
+  hideAlert() {
+    this.done = false;
+  }
+
   initializeUploader() {
-    this.auth.getAccessTokenSilently().pipe(take(1)).subscribe(token => {
+    this.done = false;
+    this.auth
+      .getAccessTokenSilently()
+      .pipe(take(1))
+      .subscribe((token) => {
         this.uploader = new FileUploader({
-          url: this.baseUrl + 'versions/add-docs/' + + this.route.snapshot.paramMap.get('id'),
+          url:
+            this.baseUrl +
+            'versions/add-docs/' +
+            +this.route.snapshot.paramMap.get('id'),
           authToken: 'Bearer ' + token,
           isHTML5: true,
-          allowedFileType: ['xls','pdf'],
+          allowedFileType: ['xls', 'pdf'],
           removeAfterUpload: true,
-          autoUpload: false
+          autoUpload: false,
         });
 
         this.uploader.onProgressAll = () => {
           this.spinnerService.show(undefined, {
             type: 'line-scale-party',
             bdColor: 'rgba(255,255,255,0)',
-            color: '#333333'
+            color: '#333333',
           });
         };
-  
+
+        this.uploader.onCompleteAll = () => {
+          this.done = true;
+        };
+
         this.uploader.onAfterAddingFile = (file) => {
           file.withCredentials = false;
         };
-  
+
         this.uploader.onCompleteItem = (item: any, status: any) => {
           this.spinnerService.hide();
           console.log('Uploaded File Details:', item?.file?.name);
         };
-    });
+      });
   }
 }
